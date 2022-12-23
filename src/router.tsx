bdb0f5eac2a351ac6a348bb2c9e55d29ee3b023d
Jameson Knoll <j.knoll621@gmail.com>
2022-12-22 23:17:08 -0500

    routing and basic setup

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
+
+export default router;
